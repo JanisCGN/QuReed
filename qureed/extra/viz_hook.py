977215df34f0b20d2bd5ab77348ecb6349de45f2
JanisCGN <45869180+JanisCGN@users.noreply.github.com>
2024-12-05 06:24:24 -0800

    Update viz_hook.py
    defined first draft of viz_hook, need to test its behaviour

--- qureed/extra/viz_hook.py
+++ qureed/extra/viz_hook.py
@@ -1,14 +1,18 @@
 """
 This module implements helpers for visualization of states during simulation
 """
-
+from photon_weave.state.composite_envelope import CompositeEnvelope
 
 
 def viz_hook(event):
   """
   this function takes an event. If the event contains a state, it displays the energy of the state.
   """
-  
+  if "signal" in event.kwargs:
+    if "envelope" in event.signal:
+      env = event.signal.envelope
+      meas = env.measure()
+      print(meas)    
   return 0
 
 
