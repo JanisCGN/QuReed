1f3e69ec2944903d800eb8a66c0e062c86813124
JanisCGN <45869180+JanisCGN@users.noreply.github.com>
2024-12-05 05:47:04 -0800

    Update and rename viz_hook to viz_hook.py

---
+++ qureed/extra/viz_hook.py
@@ -0,0 +1,15 @@
+"""
+This module implements helpers for visualization of states during simulation
+"""
+
+
+
+def viz_hook(event):
+  """
+  this function takes an event. If the event contains a state, it displays the energy of the state.
+  """
+  
+  return 0
+
+
+  
